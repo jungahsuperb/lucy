5719bbd18e20c04d5c91fce3497bc164e27bc419
jungahsuperb <jungah@superbly.cc>
2022-12-16 22:55:31 +0900

    statefullwidget 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(const MyApp());
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -9,11 +23,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //final wordPair = WordPair.random();
     return MaterialApp(
       title: 'real last!!!',
       home: Scaffold(
         appBar: AppBar(title: const Text('bye to Flutter')),
-        body: const Center(child: Text('branch test1')),
+        //body: const Center(child: Text('branch test1')),//
+        //body: Center(
+        //child: Text(wordPair.asPascalCase),
+        body: const Center(
+          child: RandomWords(),
+        ),
       ),
     );
   }
