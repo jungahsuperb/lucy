e7c882d414bd1f3a70744f60511c4fc8980555e3
jungahsuperb <jungah@superbly.cc>
2023-01-05 00:32:35 +0900

    listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,62 +7,47 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    const appName = 'Custom Themes';
-
     return MaterialApp(
-      title: appName,
-      theme: ThemeData(
-        // Define the default brightness and colors.
-        brightness: Brightness.dark,
-        primaryColor: Colors.lightBlue[800],
+        home: Scaffold(
+          appBar: AppBar(),
+          body: ListView.builder(
+            itemCount: 300,
+            itemBuilder: (c,i){
+              return ListTile(
+                leading: Icon(Icons.pages),
+                title: Text('홍길동'),
+              );
+            },
 
-        // Define the default font family.
-        fontFamily: 'Georgia',
+            ),
+          bottomNavigationBar: Homework()
 
-        // Define the default `TextTheme`. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
-        textTheme: const TextTheme(
-          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-        ),
-      ),
-      home: const MyHomePage(
-        title: appName,
-      ),
-    );
+          ),
+        );
+
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  const MyHomePage({super.key, required this.title});
+class Homework extends StatelessWidget {
+  const Homework({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Container(
-          color: Theme.of(context).colorScheme.secondary,
-          child: Text(
-            'Text with a background color',
-            style: Theme.of(context).textTheme.headline6,
-          ),
-        ),
-      ),
-      floatingActionButton: Theme(
-        data: Theme.of(context).copyWith(splashColor: Colors.yellow),
-        child: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
+
+    return BottomAppBar(
+      child: Container(
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.phone),
+            Icon(Icons.message),
+            Icon(Icons.pages)
+          ],
         ),
       ),
     );
