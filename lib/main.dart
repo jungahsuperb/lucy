dd5a3d9835b7f5dd20f5eebe27b713d1f16042cf
jungahsuperb <jungah@superbly.cc>
2023-01-11 00:15:34 +0900

    contact textfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -13,30 +15,41 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var a = 1;
-  var name = ['한상흠', '한지윤', '한재희'];
+  var name = ['joseph', 'shushu', 'kelly'];
   var like = [0, 0, 0];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-          floatingActionButton: FloatingActionButton(onPressed: (){},
-          child: Text('edd'),),
-            appBar: AppBar(),
-            body: ListView.builder(
-                itemCount: 3,
-                itemBuilder: (context, i) {
-                  return ListTile(
-                      title: Text(name[i]),
-                      leading: Text(like[i].toString()),
-                      trailing: ElevatedButton(
-                        child: Text('좋아요'),
-                        onPressed: () {
-                          setState(() {
-                            like[i]++;
-                          });
-                        },
-                      ));
-                })));
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                      content: Text(style: TextStyle(fontSize: 20), 'Contact'),
+                      actions: [
+                        TextField(),
+                        TextButton(style: TextButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 20),
+                        ),
+                        onPressed: null,
+                        child: const Text('cancel'),)
+                  ]
+                  ,
+                  );
+                });
+          },
+          child: Text('edd'),
+        ),
+        appBar: AppBar(),
+        body: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (c, i) {
+              return ListTile(
+                leading: Icon(Icons.headphones),
+                title: Text(name[i]),
+              );
+            }));
   }
 }
