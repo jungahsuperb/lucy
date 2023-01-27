caf17bff8c3a4709932fc422f145511641793f0e
jungahsuperb <jungah@superbly.cc>
2023-01-28 00:34:01 +0900

    phonebook add

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
+import 'package:contacts_service/contacts_service.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -14,15 +16,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  getPermission() async {
+    var status = await Permission.contacts.status;
+    if (status.isGranted) {
+      print('허락됨');
+      var contacts = await ContactsService.getContacts();
+      //print(contacts[0].phones);
+      setState(() {
+        name = contacts;
+      });
+
+    } else if (status.isDenied) {
+      print('거절됨');
+      Permission.contacts.request();
+    }
+  }
+
   var total = 3;
-  List<String> name = ['joseph', 'shushu', 'kelly'];
+  var name = [];
   List<String> phone = ['123', '345', '456'];
   var like = [0, 0, 0];
+  var newContact = new Contact();
+
+
+
+
+  //var contacts = await ContactsService.getContacts();
 
   addname(a) {
     setState(() {
       if (a != '') {
         name.add(a);
+        name.sort();
       }
     });
   }
@@ -41,53 +66,73 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  addnewContact(c) async{
+    setState(() {
+      if (c != ''){
+        newContact.givenName = c;
+        ContactsService.addContact(newContact);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showDialog(
-                context: context,
-                builder: (context) {
-                  return DialogUI(
-                    addOne: addOne,
-                    addName: addname,
-                    addPhone: addphone,
-                  );
-                });
-          },
-          child: Text('add'),
-        ),
-        appBar: AppBar(
-          title: Text(total.toString()),
-        ),
-        body: ListView.builder(
-            itemCount: name.length,
-            itemBuilder: (c, i) {
-              return ListTile(
-                  leading: Icon(Icons.headphones),
-                  title: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [Text(name[i]), Text(phone[i])]),
-                  //Text(name[i]),
-                  trailing: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        name.removeAt(i);
-                        phone.removeAt(i);
-                      });
-                    }, icon: Icon(Icons.delete),
-                  ));
-            }));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return DialogUI(
+                  addOne: addOne,
+                  addName: addname,
+                  addPhone: addphone,
+                  addnewContact: addnewContact,
+                );
+              });
+        },
+        child: Text('add'),
+      ),
+      appBar: AppBar(
+        title: Text(total.toString()),
+        actions: [
+          IconButton(
+              onPressed: () {
+                getPermission();
+              },
+              icon: Icon(Icons.contacts))
+        ],
+      ),
+      body: ListView.builder(
+          itemCount: name.length,
+          itemBuilder: (c, i) {
+            return ListTile(
+                leading: Icon(Icons.heart_broken),
+                title: Row(
+                  children: [Text(name[i].givenName)],
+                ),
+                //Text(name[i]),
+                trailing: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      name.removeAt(i);
+                      phone.removeAt(i);
+                    });
+                  },
+                  icon: Icon(Icons.delete),
+                ));
+          }),
+    );
   }
 }
 
 class DialogUI extends StatelessWidget {
-  DialogUI({Key? key, this.addOne, this.addName, this.addPhone})
+  DialogUI({Key? key, this.addOne, this.addName, this.addPhone, this.addnewContact,})
       : super(key: key);
   final addOne;
   final addName;
   final addPhone;
+  final addnewContact;
   var inputData = TextEditingController();
   var phoneData = TextEditingController();
 
@@ -102,15 +147,17 @@ class DialogUI extends StatelessWidget {
           children: [
             TextField(
               controller: inputData,
+              decoration: InputDecoration(hintText: 'name'),
             ),
-            TextField(
-              controller: phoneData,
-            ),
+            /*TextField(
+                controller: phoneData,
+                decoration: InputDecoration(hintText: 'phone')),*/
             TextButton(
                 onPressed: () {
                   addOne();
-                  addName(inputData.text);
-                  addPhone(phoneData.text);
+                  /*addName(inputData.text);
+                  addPhone(phoneData.text);*/
+                  addnewContact(inputData.text);
                   Navigator.pop(context);
                 },
                 child: Text('완료')),
